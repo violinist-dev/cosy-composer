24d6d0a26f24ef551e99e3ffeef05470a73cda27
Eirik S. Morland <eirik@morland.no>
2018-02-21 21:53:17 +0100

    Test no update data

---
+++ test/integration/NoUpdatesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\integration;
+
+use Composer\Console\Application;
+use eiriksm\CosyComposer\ArrayOutput;
+use eiriksm\CosyComposer\CommandExecuter;
+use Symfony\Component\Console\Input\InputDefinition;
+
+class NoUpdatesTest extends Base
+{
+    public function testNoUpdates()
+    {
+        $c = $this->getMockCosy();
+        $dir = '/tmp/' . uniqid();
+        mkdir($dir);
+        $c->setTmpDir($dir);
+        // Create a mock app, that can respond to things.
+        $mock_definition = $this->createMock(InputDefinition::class);
+        $mock_definition->method('getOptions')
+            ->willReturn([]);
+        $mock_app = $this->createMock(Application::class);
+        $mock_app->method('getDefinition')
+            ->willReturn($mock_definition);
+        $c->setApp($mock_app);
+        $mock_output = $this->createMock(ArrayOutput::class);
+        $mock_output->method('fetch')
+            ->willReturn([
+                [
+                    '{"installed": []}'
+                ]
+            ]);
+        $c->setOutput($mock_output);
+        $composer_contents = '{"require": {"drupal/core": "8.0.0"}}';
+        $composer_file = "$dir/composer.json";
+        file_put_contents($composer_file, $composer_contents);
+        $called = false;
+        $mock_executer = $this->createMock(CommandExecuter::class);
+        $mock_executer->method('executeCommand')
+            ->will($this->returnCallback(
+                function ($cmd) use (&$called) {
+                    if (strpos($cmd, 'rm -rf /tmp/') === 0) {
+                        $called = true;
+                    }
+                    return 0;
+                }
+            ));
+        $c->setExecuter($mock_executer);
+        $this->assertEquals(false, $called);
+        $c->run();
+        $this->assertEquals(true, $called);
+    }
+}
