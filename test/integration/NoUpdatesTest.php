167622c483978c20e5c9ac5aa8e7d8b0df379713
Eirik S. Morland <eirik@morland.no>
2018-02-28 16:59:16 +0100

    Add test for bad data lines in updates

--- test/integration/NoUpdatesTest.php
+++ test/integration/NoUpdatesTest.php
@@ -50,4 +50,51 @@ class NoUpdatesTest extends Base
         $c->run();
         $this->assertEquals(true, $called);
     }
+
+    public function testNoUpdatesBadDataLines()
+    {
+        $c = $this->getMockCosy();
+        $dir = '/tmp/' . uniqid();
+        mkdir($dir);
+        $c->setTmpDir($dir);
+        // Create a mock app, that can respond to things.
+        $mock_definition = $this->createMock(InputDefinition::class);
+        $mock_definition->method('getOptions')
+            ->willReturn([]);
+        $mock_app = $this->createMock(Application::class);
+        $mock_app->method('getDefinition')
+            ->willReturn($mock_definition);
+        $c->setApp($mock_app);
+        $mock_output = $this->createMock(ArrayOutput::class);
+        $mock_output->method('fetch')
+            ->willReturn([
+                [],
+                'string',
+                [
+                    'not_json:TRUE'
+                ],
+                [
+                    '{"not_installed_key": []'
+                ]
+            ]);
+        $c->setOutput($mock_output);
+        $composer_contents = '{"require": {"drupal/core": "8.0.0"}}';
+        $composer_file = "$dir/composer.json";
+        file_put_contents($composer_file, $composer_contents);
+        $called = false;
+        $mock_executer = $this->createMock(CommandExecuter::class);
+        $mock_executer->method('executeCommand')
+            ->will($this->returnCallback(
+                function ($cmd) use (&$called) {
+                    if (strpos($cmd, 'rm -rf /tmp/') === 0) {
+                        $called = true;
+                    }
+                    return 0;
+                }
+            ));
+        $c->setExecuter($mock_executer);
+        $this->assertEquals(false, $called);
+        $c->run();
+        $this->assertEquals(true, $called);
+    }
 }
