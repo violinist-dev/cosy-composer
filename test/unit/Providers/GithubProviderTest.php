94964141995b7dada918d87bfc4059d5205bf6f7
Eirik S. Morland <eirik@morland.no>
2018-02-28 17:02:06 +0100

    Add test for private authentication for github

--- test/unit/Providers/GithubProviderTest.php
+++ test/unit/Providers/GithubProviderTest.php
@@ -19,6 +19,18 @@ class GithubProviderTest extends \PHPUnit_Framework_TestCase
         $g->authenticate($user, $password);
     }
 
+    public function testAuthenticatePrivate()
+    {
+        $mock_client = $this->getMockClient();
+        $user = 'testUser';
+        $password = 'testPassword';
+        $mock_client->expects($this->once())
+            ->method('authenticate')
+            ->with($user, null, Client::AUTH_HTTP_TOKEN);
+        $g = new Github($mock_client);
+        $g->authenticatePrivate($user, $password);
+    }
+
     private function getMockClient()
     {
         return $this->createMock(Client::class);
