c5b6acc603d236c97a97093bc51b73c8866f5956
Eirik S. Morland <eirik@morland.no>
2018-03-01 07:56:36 +0100

    Add tests for repo is private or public

--- test/unit/Providers/GithubProviderTest.php
+++ test/unit/Providers/GithubProviderTest.php
@@ -3,6 +3,7 @@
 namespace eiriksm\CosyComposerTest\unit\Providers;
 
 use eiriksm\CosyComposer\Providers\Github;
+use Github\Api\Repo;
 use Github\Client;
 
 class GithubProviderTest extends \PHPUnit_Framework_TestCase
@@ -29,6 +30,46 @@ class GithubProviderTest extends \PHPUnit_Framework_TestCase
             ->with($user, null, Client::AUTH_HTTP_TOKEN);
         $g = new Github($mock_client);
         $g->authenticatePrivate($user, $password);
+    }
+
+    public function testRepoIsPrivate()
+    {
+        $user = 'testUser';
+        $repo = 'testRepo';
+        $mock_repo_api = $this->createMock(Repo::class);
+        $mock_repo_api->expects($this->once())
+            ->method('show')
+            ->with($user, $repo)
+            ->willReturn([
+                'private' => true,
+            ]);
+        $mock_client = $this->getMockClient();
+        $mock_client->expects($this->once())
+            ->method('api')
+            ->with('repo')
+            ->willReturn($mock_repo_api);
+        $g = new Github($mock_client);
+        $this->assertEquals(true, $g->repoIsPrivate($user, $repo));
+    }
+
+    public function testRepoIsPublic()
+    {
+        $user = 'testUser';
+        $repo = 'testRepo';
+        $mock_repo_api = $this->createMock(Repo::class);
+        $mock_repo_api->expects($this->once())
+            ->method('show')
+            ->with($user, $repo)
+            ->willReturn([
+                'private' => false,
+            ]);
+        $mock_client = $this->getMockClient();
+        $mock_client->expects($this->once())
+            ->method('api')
+            ->with('repo')
+            ->willReturn($mock_repo_api);
+        $g = new Github($mock_client);
+        $this->assertEquals(false, $g->repoIsPrivate($user, $repo));
     }
 
     private function getMockClient()
