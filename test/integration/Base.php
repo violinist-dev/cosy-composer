571137a2094ee5a656e7a31019407a6233ed21f7
Eirik S. Morland <eirik@morland.no>
2018-04-08 18:06:28 +0200

    Add some more helper methods for integration tests

--- test/integration/Base.php
+++ test/integration/Base.php
@@ -3,6 +3,7 @@
 namespace eiriksm\CosyComposerTest\integration;
 
 use eiriksm\ArrayOutput\ArrayOutput;
+use eiriksm\CosyComposer\CommandExecuter;
 use eiriksm\CosyComposer\CosyComposer;
 
 class Base extends \PHPUnit_Framework_TestCase
@@ -12,8 +13,23 @@ class Base extends \PHPUnit_Framework_TestCase
     {
         $app = $this->createMock('Composer\Console\Application');
         $output = $this->createMock(ArrayOutput::class);
-        $executer = $this->createMock('eiriksm\CosyComposer\CommandExecuter');
+        $executer = $this->createMock(CommandExecuter::class);
         $c = new CosyComposer('token', 'a/b', $app, $output, $executer);
         return $c;
+    }
+
+    protected function getMockExecuterWithReturnCallback($function)
+    {
+        $mock_executer = $this->createMock(CommandExecuter::class);
+        $mock_executer->method('executeCommand')
+            ->will($this->returnCallback($function));
+        return $mock_executer;
+    }
+
+    protected function createComposerFileFromFixtures($dir, $filename)
+    {
+        $composer_contents = file_get_contents(__DIR__ . "/../fixtures/$filename");
+        $composer_file = "$dir/composer.json";
+        file_put_contents($composer_file, $composer_contents);
     }
 }
