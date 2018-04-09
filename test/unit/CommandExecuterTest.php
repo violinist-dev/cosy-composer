da91c0a0edd5f716a69013f3563b3ab358fb71ea
Eirik S. Morland <eirik@morland.no>
2018-04-09 13:44:33 +0200

    Test all of command executor. Refactor the unstable assertion

--- test/unit/CommandExecuterTest.php
+++ test/unit/CommandExecuterTest.php
@@ -4,19 +4,28 @@ namespace eiriksm\CosyComposerTest\unit;
 
 use eiriksm\CosyComposer\CommandExecuter;
 use eiriksm\CosyComposer\Message;
+use eiriksm\CosyComposer\ProcessFactory;
 use PHPUnit_Framework_TestCase;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Process\Process;
 
 class CommandExecuterTest extends PHPUnit_Framework_TestCase
 {
     public function testExecuteCommand()
     {
-        $process_mock = $this->createMock(\eiriksm\CosyComposer\ProcessFactory::class);
-        $logger_mock = $this->createMock(\Psr\Log\LoggerInterface::class);
+        $process_mock = $this->createMock(ProcessFactory::class);
+        $logger_mock = $this->createMock(LoggerInterface::class);
         $command = 'echo eirik';
+        $called_correctly = false;
         $logger_mock->expects($this->once())
             ->method('log')
-            ->with('info', new Message("Creating command $command", Message::COMMAND));
+            ->willReturnCallback(function ($level, $message) use (&$called_correctly) {
+                if ($message->getMessage() == 'Creating command echo eirik'
+                    && $message->getType() == Message::COMMAND) {
+                    $called_correctly = true;
+                }
+                return true;
+            });
         $ce = new CommandExecuter($logger_mock, $process_mock);
         $process = $this->createMock(Process::class);
         $process->expects($this->once())
@@ -27,8 +36,18 @@ class CommandExecuterTest extends PHPUnit_Framework_TestCase
         $process_mock->expects($this->once())
             ->method('getProcess')
             ->willReturn($process);
+        $process->expects($this->once())
+            ->method('getOutput')
+            ->willReturn('STDOUT');
+        $process->expects($this->once())
+            ->method('getErrorOutput')
+            ->willReturn('STDERR');
         $ce->setCwd('eirik');
         $this->assertEquals($ce->getCwd(), 'eirik');
         $ce->executeCommand($command, true);
+        $output = $ce->getLastOutput();
+        $this->assertEquals('STDOUT', $output['stdout']);
+        $this->assertEquals('STDERR', $output['stderr']);
+        $this->assertEquals(true, $called_correctly);
     }
 }
