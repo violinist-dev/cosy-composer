719811386361e78d0f5be54f3c9b1d0d695c9ad2
Eirik S. Morland <eirik@morland.no>
2018-04-09 13:47:47 +0200

    Add a test for setLogger

--- test/unit/CosyComposerUnitTest.php
+++ test/unit/CosyComposerUnitTest.php
@@ -4,6 +4,7 @@ namespace eiriksm\CosyComposerTest\unit;
 
 use eiriksm\CosyComposer\CosyComposer;
 use eiriksm\CosyComposerTest\GetCosyTrait;
+use Psr\Log\LoggerInterface;
 
 class CosyComposerUnitTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,5 +16,13 @@ class CosyComposerUnitTest extends \PHPUnit_Framework_TestCase
         $test_cache_dir = '/tmp/something/here';
         $c->setCacheDir($test_cache_dir);
         $this->assertEquals($test_cache_dir, $c->getCacheDir());
+    }
+
+    public function testSetLogger()
+    {
+        $c = $this->getMockCosy();
+        $test_logger = $this->createMock(LoggerInterface::class);
+        $c->setLogger($test_logger);
+        $this->assertEquals($test_logger, $c->getLogger());
     }
 }
