22162ea64242167e8d491580267dcca3ed9530e0
Eirik S. Morland <eirik@morland.no>
2018-08-27 12:04:11 +0200

    Support private github changelog by changing clone url to non-ssh

--- src/CosyComposer.php
+++ src/CosyComposer.php
@@ -28,6 +28,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use violinist\ProjectData\ProjectData;
 use Violinist\Slug\Slug;
 use Wa72\SimpleLogger\ArrayLogger;
+use function peterpostmann\uri\parse_uri;
 
 class CosyComposer
 {
@@ -1064,6 +1065,14 @@ class CosyComposer
         // We could have this cached in the md5 of the package name.
         $clone_path = '/tmp/' . md5($data->name);
         $repo_path = $data->source->url;
+        $repo_parsed = parse_uri($repo_path);
+        if (!empty($repo_parsed)) {
+            switch ($repo_parsed['_protocol']) {
+                case 'git@github.com':
+                    $repo_path = sprintf('https://%s:%s@github.com/%s', $this->userToken, $this->githubPass, $repo_parsed['path']);
+                    break;
+            }
+        }
         if (!file_exists($clone_path)) {
             $this->execCommand(sprintf('git clone %s %s', $repo_path, $clone_path), false, 300);
         } else {
