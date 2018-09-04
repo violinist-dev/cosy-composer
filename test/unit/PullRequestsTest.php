1773b3bdb4e840b11c1486f035a67f725852b910
Eirik S. Morland <eirik@morland.no>
2018-09-04 21:00:56 +0200

    Add a unit test for the trimmed title

---
+++ test/unit/PullRequestsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace eiriksm\CosyComposerTest\unit;
+
+
+use eiriksm\CosyComposer\CosyComposer;
+use eiriksm\CosyComposerTest\GetCosyTrait;
+use eiriksm\ViolinistMessages\ViolinistMessages;
+
+class PullRequestsTest extends \PHPUnit_Framework_TestCase
+{
+    use GetCosyTrait;
+
+    public function testPullrequestTitle()
+    {
+        // Use reflection to invoke the protected method we want to test.
+        $class = new \ReflectionClass(CosyComposer::class);
+        $method = $class->getMethod('createTitle');
+        $method->setAccessible(true);
+        $mock_cosy = $this->getMockCosy();
+        $item = (object) [
+            'name' => 'test/package',
+            'version' => '1.0.0',
+        ];
+        $post_update = (object) [
+            // I mean, even if we have a newline.
+            'version' => "1.0.1\n",
+        ];
+
+        $title = $method->invokeArgs($mock_cosy, [$item, $post_update]);
+        $this->assertEquals('Update test/package from 1.0.0 to 1.0.1', $title);
+    }
+}
